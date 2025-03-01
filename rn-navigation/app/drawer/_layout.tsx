496a70a654529a5dd2d4f7738a1b8f5b28b31f3b
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2025-03-01 18:38:07 +0530

    Update drawer styles

--- rn-navigation/app/drawer/_layout.tsx
+++ rn-navigation/app/drawer/_layout.tsx
@@ -1,19 +1,38 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { withLayoutContext } from 'expo-router';
 import { COLORS } from '@/constants/colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const DrawerNavigator = withLayoutContext(createDrawerNavigator().Navigator);
 
 const screenOptions = {
   headerStyle: { backgroundColor: COLORS.GREY_3 },
   headerTintColor: COLORS.DARK,
+  drawerActiveTintColor: COLORS.DARK,
+  drawerInactiveTintColor: COLORS.GREY_2,
 };
 
 const DrawerLayout = () => {
   return (
     <DrawerNavigator screenOptions={screenOptions}>
-      <DrawerNavigator.Screen name='categories' options={{ title: 'All Categories' }} />
-      <DrawerNavigator.Screen name='favourites' options={{ title: 'Favourites' }} />
+      <DrawerNavigator.Screen
+        name='categories'
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }: { color: string; size: number }) => (
+            <Ionicons name='list' color={color} size={size} />
+          ),
+        }}
+      />
+      <DrawerNavigator.Screen
+        name='favourites'
+        options={{
+          title: 'Favourites',
+          drawerIcon: ({ color, size }: { color: string; size: number }) => (
+            <Ionicons name='star' color={color} size={size} />
+          ),
+        }}
+      />
     </DrawerNavigator>
   );
 };
