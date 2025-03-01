9b87294674e05c538afb805e9b25314f051be5ec
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2025-03-01 18:56:02 +0530

    Create favourites context for global state management of favourite meals

--- rn-navigation/app/_layout.tsx
+++ rn-navigation/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { COLORS } from '@/constants/colors';
+import FavouritesContextProvider from '@/store/context/FavouritesContext';
 
 const screenOptions = {
   headerStyle: { backgroundColor: COLORS.GREY_3 },
@@ -9,12 +10,14 @@ const screenOptions = {
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack screenOptions={screenOptions}>
-        <Stack.Screen name='drawer' options={{ headerShown: false, title: '' }} />
-        <Stack.Screen name='category/[category]/index' options={{ title: 'Meals' }} />
-        <Stack.Screen name='category/[category]/[meal]/index' options={{ title: 'Meal' }} />
-      </Stack>
-    </GestureHandlerRootView>
+    <FavouritesContextProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack screenOptions={screenOptions}>
+          <Stack.Screen name='drawer' options={{ headerShown: false, title: '' }} />
+          <Stack.Screen name='category/[category]/index' options={{ title: 'Meals' }} />
+          <Stack.Screen name='category/[category]/[meal]/index' options={{ title: 'Meal' }} />
+        </Stack>
+      </GestureHandlerRootView>
+    </FavouritesContextProvider>
   );
 }
---
+++ rn-navigation/store/context/FavouritesContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+type FavouritesContextType = {
+  ids: Array<string>;
+  addFavourite: (id: string) => void;
+  removeFavourite: (id: string) => void;
+};
+
+const FavouritesContext = createContext<FavouritesContextType>({
+  ids: [],
+  addFavourite: (id: string) => {},
+  removeFavourite: (id: string) => {},
+});
+
+const FavouritesContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [ids, setIds] = useState<Array<string>>([]);
+
+  const addFavourite = (id: string) => {
+    setIds(prevIds => [...prevIds, id]);
+  };
+
+  const removeFavourite = (id: string) => {
+    setIds(prevIds => prevIds.filter(favouriteId => favouriteId !== id));
+  };
+
+  return (
+    <FavouritesContext.Provider value={{ ids, addFavourite, removeFavourite }}>{children}</FavouritesContext.Provider>
+  );
+};
+
+export default FavouritesContextProvider;
